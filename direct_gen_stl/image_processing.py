f9a5e9fe521dd7120e9ca6334e78b2ce88894581
naysok <ysok.ysok@gmail.com>
2020-02-20 11:57:52 +0900

    [update] space

--- direct_gen_stl/image_processing.py
+++ direct_gen_stl/image_processing.py
@@ -11,15 +11,12 @@ class ImageProcessing():
     def get_color_to_memory(self, path):
 
         img = Image.open(path)
-
         size = img.size
 
         cl_l = []
 
         for i in range(size[1]):
-
             cl = []
-
             for j in range(size[0]):
                 c = img.getpixel((j, i))
                 cl.append(c)
@@ -32,30 +29,27 @@ class ImageProcessing():
     def get_mask(self, path, threshold):
 
         img = Image.open(path)
-
         size = img.size
 
         mask = []
 
         for i in range(size[1]):
-
             sub = []
-
             for j in range(size[0]):
+                
                 
                 c = img.getpixel((j, i))
                 value = c[0]
 
-                ### null
+                ### Ture
                 if value >> threshold:
                     sub.append(1)
                     # print("1")
-                
+
                 ### masked
                 else:
                     sub.append(0)
                     # print("0")
-
             
             mask.append(sub)
         
