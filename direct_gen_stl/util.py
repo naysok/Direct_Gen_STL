db0d995f0425353e7c26c60cb3300e9ebd6d6fde
naysok <ysok.ysok@gmail.com>
2020-02-20 11:54:59 +0900

    [update] move point, pvm

--- direct_gen_stl/util.py
+++ direct_gen_stl/util.py
@@ -14,6 +14,14 @@ class UTIL():
         return pt
 
 
+    def move_pt_vec(self, pt, vec):
+        xx = pt[0] + vec[0]
+        yy = pt[1] + vec[1]
+        zz = pt[2] + vec[2]
+        p = [xx, yy, zz]
+        return p
+
+
     def face_normal(self, p0, p1, p2):
         v1 = self.pt_pt_subtract(p1, p0)
         v2 = self.pt_pt_subtract(p2, p0)
@@ -30,3 +38,24 @@ class UTIL():
 
     def remap_number(self, src, old_min, old_max, new_min, new_max):
         return ((src - old_min) / (old_max - old_min) * (new_max - new_min) + new_min)
+
+    
+    def point_vector_mask(self, list_pvm):
+
+        pp = []
+        vv = []
+        mm = []
+
+        for i in range(len(list_pvm)):
+
+            pvm = list_pvm[i]
+            p_v_m = pvm.split(',')
+            p = [p_v_m[0], p_v_m[1], p_v_m[2]]
+            v = [p_v_m[3], p_v_m[4], p_v_m[5]]
+            m = p_v_m[6]
+
+            pp.append(p)
+            vv.append(v)
+            mm.append(m)
+
+        return pp, vv, mm
